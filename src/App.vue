cb9ec6bacc7773c5762a38a66e15385aad40e878
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-12 18:20:59 +0200

    modify project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <AppHero />
+  <AppTitle />
 </template>
 
 <script>
-  import AppHero from './components/AppHero.vue';
+  import AppTitle from './components/AppTitle.vue'
   export default {
     name: 'App',
     components: {
-      AppTitle,
-      AppHero
+      AppTitle
+
     }
   }
 </script>
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -11,7 +11,7 @@
         name: 'AppHero',
         data() {
             return {
-                btnText: 'Click me!'
+                
             }
         }
     }
@@ -19,13 +19,13 @@
 
 <style lang="scss" scoped>
 
-  .hero-image {
+.hero-image {
     background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../assets/img/jumbo.jpg");
     height: 50%;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
     position: relative;
-  }
+}
 
 </style>
