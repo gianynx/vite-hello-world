dc6d399cade231feceb228010a4fb7419487aae7
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-12 16:19:15 +0200

    create AppTitle component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ title }}</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
