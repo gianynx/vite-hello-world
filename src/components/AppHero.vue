80157891b55e87936c6a5fcfed5e3701f1744180
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-12 17:23:50 +0200

    add AppHero.vue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="hero-image"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data() {
+            return {
+                btnText: 'Click me!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+  .hero-image {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../assets/img/jumbo.jpg");
+    height: 50%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  }
+
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -18,5 +18,12 @@
 </script>
 
 <style lang="scss" scoped>
-
+h1 {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+}
 </style>
